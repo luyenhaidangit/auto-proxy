957deeeef0472eadfde1a3d41701ea07ec5ff634
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-07-14 05:26:27 +0700

    Commit

--- src/AutoProxy.Api/Persistence/AutoProxyDbContext.cs
+++ src/AutoProxy.Api/Persistence/AutoProxyDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace AutoProxy.Api.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AutoProxy.Api.Persistence
 {
-    public class AutoProxyDbContext
+    public class AutoProxyDbContext : DbContext
     {
+        public AutoProxyDbContext(DbContextOptions<AutoProxyDbContext> options) : base(options)
+        {
+        }
+
+        #region DbSet
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+
+        }
     }
 }
