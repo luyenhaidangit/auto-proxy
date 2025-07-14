366e2cce833e87d650a545fb6aac5e5a3dde885b
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-07-14 21:43:39 +0700

    Commit

---
+++ src/AutoProxy.Shared/Constants/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace AutoProxy.Shared.Constants
+{
+    public class Message
+    {
+        public const string Success = "Success";
+        public const string Failure = "Failure";
+    }
+}
